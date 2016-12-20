f44bf0d18a893cf80eeed557d3760655241a70d4
aquoss <amberquoss@gmail.com>
2016-12-19 16:31:45 -0800

    creates new song model embedded in albums

--- models/albums.js
+++ models/albums.js
@@ -5,7 +5,8 @@ var AlbumSchema = new Schema({
     artistName: String,
     name: String,
     releaseDate: String,
-    genres: [String]
+    genres: [String],
+    songs: [SongSchema]
 });
 
 var Album = mongoose.model('Album', AlbumSchema);
